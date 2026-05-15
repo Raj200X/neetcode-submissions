948a658111c2a0bd99911674d3877510c9ae4af8
Raj Srivastava <91322992+Raj200X@users.noreply.github.com>
2026-05-15 15:05:01 +0530

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    void sortColors(vector<int>& nums) {
+        int z = 0;
+        int o = 0;
+        int t = 0;
+        for (int i =0;i<nums.size();i++){
+            if (nums[i]==0){
+                z++;
+            }
+            else if (nums[i]==1){
+                o++;
+            }
+            else{
+                t++;
+            }
+        }
+        int i = 0;
+        while (z--) nums[i++] =0;
+        while (o--) nums[i++] =1;
+        while (t--) nums[i++] =2;
+
+        
+        
+    }
+};
